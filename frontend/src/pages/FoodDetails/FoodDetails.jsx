b51b9b89ed148a1c4be6723e3f21b06aca4aa452
Amarnadh001 <165272726+Amarnadh001@users.noreply.github.com>
2025-03-01 22:20:52 +0530

    Update FoodDetails.jsx

--- frontend/src/pages/FoodDetails/FoodDetails.jsx
+++ frontend/src/pages/FoodDetails/FoodDetails.jsx
@@ -12,7 +12,7 @@ const FoodDetails = () => {
   useEffect(() => {
     const fetchFoodDetails = async () => {
       try {
-        const response = await axios.get(`http://localhost:4000/api/food/${id}`);
+        const response = await axios.get(`https://foodfusion-backend-lfj9.onrender.com/api/food/${id}`);
         if (response.data.success) {
           setFood(response.data.data);
         } else {
