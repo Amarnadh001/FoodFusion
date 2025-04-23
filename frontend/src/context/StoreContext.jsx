1622dfce4b8508ebf7dfab19417738c607958a53
Amarnadh001 <99220041612@klu.ac.in>
2025-04-23 11:30:53 +0530

    1

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
     const navigate = useNavigate();
     const [cartItems, setCartItems] = useState({});
-    const url = "http://localhost:4000";
+    const url = "https://foodfusion-backend-lfj9.onrender.com";
     const [token, setToken] = useState("");
     const [userId, setUserId] = useState("");
     const [food_list, setFoodList] = useState([]);
